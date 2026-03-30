aea55d585ba4dba9e72a80b65c484612e307f313
Faisal Khan <faisal01khan2003@gmail.com>
2026-03-30 17:46:58 +0530

    add category-microservice

---
+++ category-service/src/main/java/io/github/faisalkhan25/categoryService/CategoryServiceApplication.java
@@ -0,0 +1,13 @@
+package io.github.faisalkhan25.categoryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CategoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CategoryServiceApplication.class, args);
+	}
+
+}
